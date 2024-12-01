6b4ff1c6255f225c10c0e0056ac64241e0fc2fe6
Jimi Herken <mail@jimi.berlin>
2024-12-01 12:43:24 +0100

    Added day 1 solutions

---
+++ day-1/part1.ts
@@ -0,0 +1,23 @@
+const input = Deno.readTextFileSync('input.txt')
+                .split('\r\n')
+                .map(el => el.split('   '));
+
+const left = [], right = [];
+
+for (const line of input) {
+    left.push(parseFloat(line[0]));
+    right.push(parseFloat(line[1]));
+}
+
+left.sort();
+right.sort();
+
+let distanceSum = 0;
+
+for (let i = 0; i < left.length; i++) {
+    const distance = Math.abs(left[i] - right[i]);
+
+    distanceSum += distance;
+}
+
+console.log(distanceSum);
---
+++ day-1/part2.ts
@@ -0,0 +1,24 @@
+const input = Deno.readTextFileSync('input.txt')
+                .split('\r\n')
+                .map(el => el.split('   '));
+
+const left = [], right = [];
+
+for (const line of input) {
+    left.push(parseFloat(line[0]));
+    right.push(parseFloat(line[1]));
+}
+
+let similarityScore = 0;
+
+for (const element of left) {
+    let occurences = 0;
+
+    for (const rightElement of right) {
+        if (rightElement == element) occurences++;
+    }
+
+    similarityScore += (element * occurences);
+}
+
+console.log(similarityScore);
